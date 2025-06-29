d57f3e393c434084009d6df7bb8d1a98285c75cf
swastikaa0 <swastikadhungana2004@gmail.com>
2025-06-29 20:34:05 +0545

    backend

---
+++ Backend/index.js
@@ -0,0 +1,36 @@
+// const express=require("express");
+// const {connection } = require("./src/Database");
+
+
+// const app = express();
+
+// const PORT = 4000;
+
+// app.get("/",(req,res)=>{
+//     res.send('Sever running on port ${PORT}');
+
+// });
+// connection();
+
+// app.listen(PORT,() =>{
+//     console.log('Server running on port ${PORT}');
+// });
+import express from 'express'
+import bodyParser  from 'body-parser';
+import { db } from './src/Database/index.js';
+import {userRouter} from './src/route/userRoute.js'
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+
+const app=express();
+
+const port=process.env.PORT||5000
+app.use(bodyParser.json());
+app.use('/api/users',userRouter)
+
+app.listen(5000,function(){
+  console.log("project running in port ")
+  db()
+})
---
+++ Backend/src/route/userRoute.js
@@ -0,0 +1,12 @@
+import express from 'express'
+import { userController } from '../../controller/index.js';
+const router=express.Router();
+router.get("/",userController.getAll);
+router.post("/",userController.create);
+router.patch("/:id",userController.update);
+router.get("/:id",userController.getById);
+router.delete("/:id",userController.delelteById);
+
+
+
+export  {router as userRouter };
